a1762700d2b25634752d86b2a3d16257c77e3500
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-19 18:57:48 -0500

    update for project 3

--- src/test/java/com/nttdata/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/nttdata/apigateway/ApiGatewayApplicationTests.java
@@ -1,12 +1,10 @@
 package com.nttdata.apigateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
